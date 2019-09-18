237c3119e3c8ae19fbbe2c41598d4788499640fa
Ian Moffitt <IMoffitt@barkleyrei.com>
2019-09-18 09:07:26 -0400

    feat: Test improvements and additions.

--- test/test-app.js
+++ test/test-app.js
@@ -6,6 +6,7 @@ const os = require('os');
 const util = require('../lib/utils.js');
 const exec = require('child_process').exec;
 const assert = require('yeoman-assert');
+const fs = require('fs');
 
 // Global vars
 let build_error_code = 0;
@@ -16,11 +17,21 @@ let build_error_stdout = '';
  * Test basic file generation,
  * including that from brei-assemble-structure, brei-assemble-helpers, brei-sass-boilerplate.
  */
-describe('Main Generator', function () {
+describe('Generator Files', function () {
 	'use strict';
 
+	it('Generator self check', function () {
+		let dir = path.join(__dirname, '../');
+		util._test_brei_generator_files(dir);
+	});
+});
+
+describe('Generator Functionality', function () {
+	'use strict';
+
+	let tdir = path.join(os.tmpdir(), './temp');
+
 	before(function mainGenerator(done) {
-		let tdir = path.join(os.tmpdir(), './temp');
 		this.timeout(120000);
 
 		console.log('\nRunning a generator with npm install. This might take a while...\n\n');
@@ -57,7 +68,6 @@ describe('Main Generator', function () {
 	});
 
 	it('Build finished with an error code of 0', function () {
-
 		if ('0' !== build_error_code.toString()) {
 			console.log('\n\n -- ERROR --\n');
 			console.error(build_error_msg);
@@ -69,51 +79,97 @@ describe('Main Generator', function () {
 	});
 
 	it('Standard project setup was created with all files and folders we expect', function () {
-
-		let tdir = path.join(os.tmpdir(), './temp/');
 		util._test_brei_npm_built_files(tdir);
+	});
+
+	it('Template Sub-Generator', function () {
+		util._test_sub_generators('template', tdir);
+	});
+
+	it('Organism Sub-Generator', function () {
+		util._test_sub_generators('organism', tdir);
+	});
+
+	it('Molecule Sub-Generator', function () {
+		util._test_sub_generators('molecule', tdir);
+	});
+
+	it('Atom Sub-Generator', function () {
+		util._test_sub_generators('atom', tdir);
+	});
+
+	it('Partial (Legacy) Sub-Generator', function () {
+		util._test_sub_generators('partial', tdir);
+	});
+
+	it('Module (Legacy) Sub-Generator', function () {
+		util._test_sub_generators('module', tdir);
+	});
+
+	it('Test updateScss.js', function (done) {
+		let filePath = path.join(tdir, 'app/assemble/no-scss.hbs');
+
+		fs.writeFile(filePath, 'no-scss', function (err) {
+			if (err) {
+				throw err;
+			}
+
+			exec('npm run assemble:execute', {
+				cwd: tdir
+			}, function (error, stdout, stderr) {
+				if (error !== null) {
+					if (error.code !== null) {
+						if ('0' !== error.code.toString()) {
+
+							assert.file([
+								path.join(tdir, 'app/assemble/no-scss.hbs'),
+								path.join(tdir, 'app/sass/templates/_no-scss.scss')
+							]);
+
+							assert.fileContent(path.join(tdir, 'app/sass/templates/_assemble-templates.scss'), /@import\s+"home-page";\n@import\s+"test-template.scss";\n@import\s+"no-scss";/);
+
+						}
+					}
+				}
+
+				done();
+			});
+
+		});
+	});
+
+	it('Test copy.js capturing extraneous files', function (done) {
+
+		let randoFile = Math.random().toString(36).substring(2, 15) + '.txt';
+		let randoDir = Math.random().toString(36).substring(2, 15);
+
+		let writeDir = path.join(tdir, randoDir);
+
+		fs.writeFile(writeDir, 'test random file', function (err) {
+			if (err) {
+				throw err;
+			}
+
+			exec('npm run copy', {
+				cwd: tdir
+			}, function (error, stdout, stderr) {
+				if (error !== null) {
+					if (error.code !== null) {
+						if ('0' !== error.code.toString()) {
+
+							assert.file([
+								path.join(tdir, 'dist/' + randoDir + '/' + randoFile)
+							]);
+
+						}
+					}
+				}
+
+				done();
+			});
+		});
 
 	});
 
 });
 
-describe('Check Generator Files', function () {
-	'use strict';
-
-	it('Generator self check', function () {
-		let dir = path.join(__dirname, '../');
-		util._test_brei_generator_files(dir);
-	});
-});
-
-// Atomic Generators
-describe('Template Sub-Generator', function () {
-	'use strict';
-	util._test_sub_generators('template');
-});
-
-describe('Organism Sub-Generator - ', function () {
-	'use strict';
-	util._test_sub_generators('organism');
-});
-
-describe('Molecule Sub-Generator - ', function () {
-	'use strict';
-	util._test_sub_generators('molecule');
-});
-
-describe('Atom Sub-Generator - ', function () {
-	'use strict';
-	util._test_sub_generators('atom');
-});
-
-// Legacy Generators
-describe('Module (Legacy) Sub-Generator - ', function () {
-	'use strict';
-	util._test_sub_generators('module');
-});
-
-describe('Partial (Legacy) Sub-Generator - ', function () {
-	'use strict';
-	util._test_sub_generators('partial');
-});
